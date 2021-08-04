043f849b9b9fcc8a2e7d87abc7d23bab6d923412
Sasha Petrov <brat77@tut.by>
2021-08-04 19:01:35 +0300

    feat: decide task

--- src/problems/00-task.js
+++ src/problems/00-task.js
@@ -2,25 +2,29 @@ const task0 = {
     //вернуть сумму 3х чисел
     sum(a, b, c) {
         let sum = 0;
-        //ваш код тут
+        sum = a+b+c; //ваш код тут
         return sum;
     },
     //вернуть сумму чисел, но надо учесть что аргументы могут быть не только числами, использовать оператор typeof в связке с if-else
     safeSum(a, b, c) {
         let sum = 0;
-        //ваш код тут;
+        if (typeof a === "number")   sum += a;//ваш код тут
+        if (typeof b === "number")   sum += b;
+        if (typeof c === "number")   sum += c;
         return sum;
     },
     // вернуть максимальное число
     max(a, b, c) {
         let m = a;
-        //ваш код тут
+        if (m < b) m = b;//ваш код тут
+        if (m < c) m = c;
         return m;
     },
     //вернуть минимальное число
     min(a, b, c) {
         let m = a;
-        //ваш код тут
+        if (m > b) m = b;//ваш код тут
+        if (m > c) m = c;
         return m;
     }
 };
