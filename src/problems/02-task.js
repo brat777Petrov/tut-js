a451d904c48bc8a959e190c414dbf97c75f99309
Sasha Petrov <brat77@tut.by>
2021-08-05 18:25:35 +0300

    feat:decide task

--- src/problems/02-task.js
+++ src/problems/02-task.js
@@ -14,13 +14,41 @@ const task2 = {
     * *********
      */
     printInFrame: function(srcString, ch) {
-
+      let arrayWord = srcString.split(' ');
+      
+      //*** maxLength ***
+      let lineChar = '';
+      let arrayChar = [];
+      let maxLengthEl = 0;
+      for ( let i = 0; i < arrayWord.length; i++ ) {
+        arrayChar = arrayWord[i].split('');
+        if (arrayChar.length > maxLengthEl) maxLengthEl = arrayChar.length;
+      };
+      for ( let i = 0; i < (maxLengthEl + 4); i++ ) {
+        lineChar += ch;
+      }
+     
+      //*** print frame ***/
+      console.log(lineChar);
+      let longSpace = '';
+      for ( let i = 0; i < arrayWord.length; i++ ) {
+        for ( let j = 0; j < (maxLengthEl - arrayWord[i].length + 1); j++ ){
+          longSpace += ' '; 
+        };
+        console.log(ch + ' ' + arrayWord[i] + longSpace + ch);
+        longSpace = '';
+      }
+      console.log(lineChar);
     },
 
     /*
     reverseString('abc') => 'cba';
      */
     reverseString(str) {
+      let arr = str.split('');
+      arr.reverse();
+      let strNew = arr.join('');
+      return strNew;
 
     },
     /*
@@ -31,18 +59,41 @@ const task2 = {
     reverseNumber(-345) => -543
      */
     reverseNumber: function(n) {
-
+      let arr = n.split('');
+      arr.reverse();
+  
+      if ( n < 0) {
+        arr.pop(arr[arr.length]);
+        arr.unshift('-');
+      }
+      
+      let numNew = arr.join('');
+      return numNew;
     },
     /*
     capitalize('this string will be capitalized') => 'This String Will Be Capitalized'
      */
     capitalizeWords: function(str) {
-
+      let arrWords = str.split(' ');
+      let arrChars = [];
+      let char;
+      for ( let i = 0; i < arrWords.length; i++ ) {
+        arrChars = arrWords[i].split('');
+        char = arrChars[0]
+        arrChars[0] = char.toUpperCase();
+        arrWords[i] = arrChars.join('');
+      }
+      return arrWords.join(' ');
     },
     /*
     chunk([1, 2, 3, 4, 5], 3) => [[1, 2, 3], [4, 5]]
      */
     chunk: function(arr, chunkSize) {
-
+      let arrLength = Math.ceil(arr.length/chunkSize);
+      let arrNew = [];
+      for ( i = ( arrLength - 1 ); i >= 0; i-- ) {
+        arrNew[i] = arr.splice(i*chunkSize);
+      }
+      return arrNew
     }
 };
