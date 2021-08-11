aece06b48e22c69fcc18196a862060c49b72b10c
Sasha Petrov <brat77@tut.by>
2021-08-11 19:58:13 +0300

    decide task

--- src/problems/tasks/objects.js
+++ src/problems/tasks/objects.js
@@ -3,7 +3,12 @@ const objectsTask = {
     //вернуть новый объект, что-бы в нём ключи и значения были поменяны местами
     //swapKeysWithValues({a: 'aa', b: 'bb'}) => { aa: 'a', bb: 'b' };
     swapKeysWithValues(obj) {
+      let objNew = {};
 
+      for (let key in obj) {
+         objNew[obj[key]] = key;
+      }
+      return objNew;
     },
 
 };
